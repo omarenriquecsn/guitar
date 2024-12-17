cdb8d5584c749eb182bfaa2cc2fa26d0d5c1c726
unknown <omarenriquecs@gmail.com>
2024-12-17 13:25:46 -0400

    guitar

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import Guitar from "./components/Guitar"
+import Header from "./components/Header"
+import { useState, useEffect } from "react"
+import { db } from "./data/db"
+
+
+function App() {
+
+    const [data, setData] = useState(db)
+    const [cart, setCart] = useState([])
+
+
+    return (
+        <>
+            <Header />
+
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
+
+                <div className="row mt-5">
+                    {data.map((guitar) => (
+                    
+                        <Guitar
+                        key={guitar.id}
+                        cart={cart}
+                        guitar={guitar}
+                        setCart={setCart}
+                        />
+                    )
+                    )}
+
+                </div>
+            </main>
+
+
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-x1">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+
+        </>
+    )
+}
+
+export default App
